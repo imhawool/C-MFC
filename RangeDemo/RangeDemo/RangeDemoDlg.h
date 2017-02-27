d6c1bca10511fe286d8f457814ce9a98fcd29fac
imhawool <ektmfrlehdrh@gmail.com>
2017-02-28 08:50:58 +0900

    C++/MFC열혈강의14장

---
+++ RangeDemo/RangeDemo/RangeDemoDlg.h
@@ -0,0 +1,39 @@
+
+// RangeDemoDlg.h : 헤더 파일
+//
+
+#pragma once
+#include "afxcmn.h"
+
+
+// CRangeDemoDlg 대화 상자
+class CRangeDemoDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CRangeDemoDlg(CWnd* pParent = NULL);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+	enum { IDD = IDD_RANGEDEMO_DIALOG };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CProgressCtrl m_Progress;
+	CSliderCtrl m_Slider;
+	afx_msg void OnNMReleasedcaptureSlider(NMHDR *pNMHDR, LRESULT *pResult);
+	afx_msg void OnMouseMove(UINT nFlags, CPoint point);
+	afx_msg void OnNMCustomdrawSlider(NMHDR *pNMHDR, LRESULT *pResult);
+};
